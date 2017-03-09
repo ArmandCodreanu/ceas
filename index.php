08e55379db7c86b512a050bd7eb8d60129e664ed
ArmandCodreanu <cod_armi@yahoo.com>
2017-03-10 00:22:49 +0200

    FIRST - only top made

---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+    <head>
+        <title>Ceasuri</title>
+        <link rel="stylesheet" href="css/fonts.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+       <!-- HEADER -->
+        <header>
+            <div class="wrapper">
+               <!-- LOGO -->
+                <div class="logo-holder">
+                    <img src="img/logo.png" alt="Ceasuri" title="Logo" class="logo">
+                </div>
+                <!-- EOF LOGO -->
+                <!-- NAV -->
+                <nav>
+                    <ul class="nav">
+                        <li><a href=""><span>&#8962;</span> HOME</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Build your own</a></li>
+                    </ul>
+                </nav>
+                <!-- EOF NAV -->
+                <div class="clear"></div>
+            </div>
+        </header>
+        <!-- EOF HEADER -->
+        <!-- MAIN -->
+        <main>
+            <!-- FULL SIZE IMAGE -->
+            <div class="full-image">
+                <div class="motto">
+                    <span>&nbsp;Our watches are the highest quality ever!&nbsp;<br>
+                    &nbsp;It's the perfect gift.&nbsp;</span>
+                    <a href="">Order now</a>
+                </div>
+                <div class="scroll-me">
+                    <a href=""><img src="img/scroll.gif" alt="Scroll to continue"></a>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <!-- EOF FULL SIZE IMAGE -->
+            <!-- TOP WATCHES -->
+            <div class="top">
+                <div class="wrapper">
+                    <h1>Bestseller watches</h1>
+                    <div class="top-holder"></div>
+                </div>
+            </div>
+            <!-- EOF TOP WATCHES -->
+        </main>
+        <!-- EOF MAIN -->
+        <!-- FOOTER -->
+        <footer>
+            <div class="wrapper">
+                Footer
+            </div>
+        </footer>
+        <!-- EOF FOOTER -->
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script>
+            $(document).ready(function(){
+
+            });
+        </script>
+    </body>
+</html>
